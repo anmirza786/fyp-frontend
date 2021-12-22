34639ba1aa7057ad40e206ff1bd46cf3de7ecf99
Tayyabahm <ahmedtayyab533@gmail.com>
2021-12-22 13:33:46 +0500

    Front end

---
+++ src/components/Navbars/AuthNavbar.js
@@ -0,0 +1,116 @@
+/*eslint-disable*/
+import React from "react";
+import { Link } from "react-router-dom";
+
+// components
+
+import IndexDropdown from "components/Dropdowns/IndexDropdown.js";
+// import PagesDropdown from "components/Dropdowns/PagesDropdown.js";
+
+export default function Navbar(props) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+      <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link
+              className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+              to="/"
+            >
+              Gift Redeemer & Store
+            </Link>
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="text-white fas fa-bars"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" +
+              (navbarOpen ? " block rounded shadow-lg" : " hidden")
+            }
+            id="example-navbar-warning"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              {/* <li className="flex items-center">
+                <PagesDropdown />
+              </li> */}
+              <li className="flex items-center">
+                <Link to="/competitions">
+                  <a
+                    className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                    href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-react%2F%23%2F"
+                    target="_blank"
+                  >
+                    Competitions
+                  </a>
+                </Link>
+              </li>
+
+              <li className="flex items-center">
+                <Link to="/HowToPlay">
+                  <a
+                    className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                    target="_blank"
+                  >
+                    How To Play
+                  </a>
+                </Link>
+              </li>
+
+              <li className="flex items-center">
+                <Link to="/winners">
+                  <a
+                    className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                    target="_blank"
+                  >
+                    Winners
+                  </a>
+                </Link>
+              </li>
+
+              <li className="flex items-center">
+                <Link to="/winners">
+                  <a
+                    className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                    target="_blank"
+                  >
+                    Shop
+                    {/* <span className="lg:hidden inline-block ml-2">Tweet</span> */}
+                  </a>
+                </Link>
+              </li>
+
+              {/* <li className="flex items-center">
+                <button
+                  className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                  type="button"
+                >
+                  My Account
+                </button>
+              </li> */}
+
+              <li className="flex items-center">
+                <IndexDropdown />
+              </li>
+
+              <li className="flex items-center">
+                <a
+                  className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  href="https://github.com/creativetimofficial/notus-react?ref=nr-auth-navbar"
+                  target="_blank"
+                >
+                  Cart
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
